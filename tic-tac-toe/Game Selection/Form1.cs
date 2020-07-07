face422ea3871815d0ba9f571144bfb9f5b7fcbf
Hayden Gill <H.Gill@SPS.local>
2020-07-07 22:09:20 +0100

    Initial game selection project

---
+++ tic-tac-toe/Game Selection/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tic_tac_toe;
+
+namespace Game_Selection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tictactoe_Click(object sender, EventArgs e)
+        {
+            tic_tac_toe.Form1 ttt = new tic_tac_toe.Form1();
+            Show();
+                       
+        }
+    }
+}
