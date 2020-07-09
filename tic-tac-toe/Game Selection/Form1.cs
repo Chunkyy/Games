0ac9f2a4bbddaeb98516c804ccbdbb83cf7c527d
Hayden Gill <H.Gill@SPS.local>
2020-07-09 20:54:04 +0100

    Game Select application merged into branch 001

---
+++ tic-tac-toe/Game Selection/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tic_tac_toe;
+
+namespace Game_Selection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        tic_tac_toe.Form1 ttt = new tic_tac_toe.Form1();
+        private void tictactoe_Click(object sender, EventArgs e)
+        {
+            
+            if (ttt.Visible == true)
+            {
+                ttt.BringToFront();
+            }
+            else
+            {
+                ttt.Show();
+            }
+        }
+    }
+}
